3ce5731d7e70fd896f25d1fb3d53c3fcfdb773e3
nertexisdead <110025832+nertexisdead@users.noreply.github.com>
2025-04-04 09:25:27 +0800

    Added docker (#4)
    * - Added base structure files
    
    * - Final docker settings
    
    * - Fixed jquery error
    
    * - Fixed livewire import
    
    * - Fixed readme

--- application/resources/views/posts/edit.blade.php
+++ application/resources/views/posts/edit.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.app')
 @section('content')
 
-    <form data-action="{{ route('posts.save') }}" class="new_post_form">
+    <form data-action="{{ route('posts.update', ['post' => $post]) }}" class="edit_post_form">
         <div class="card card-success mt-4">
             <div class="card-header">
                 <div class="row justify-content-between align-items-center">
                     <div class="col-6 center-align">
-                        <h3 class="card-title">{{__('Create post')}}</h3>
+                        <h3 class="card-title">{{__('Edit post')}}</h3>
                     </div>
                 </div>
             </div>
             <div class="card-body">
                 <div class="form-group">
                     <label>{{__('Post: title')}}</label>
-                    <input name="title" type="text" class="form-control" placeholder="{{__('Enter post title')}}">
+                    <input name="title" type="text" class="form-control" placeholder="{{__('Enter post title')}}"
+                           value="{{ $post->title }}">
                     <span class="error title"></span>
                 </div>
                 <div class="form-group">
                     <label>{{__('Post: content')}}</label>
                     <textarea name="content" type="text" class="form-control js-posts-editor"
-                              placeholder="{{__('Enter post content')}}" rows="4"></textarea>
+                              placeholder="{{__('Enter post content')}}" rows="4">{{ $post->content }}</textarea>
                     <span class="error content"></span>
                 </div>
             </div>
             <div class="card-footer">
-                <button type="submit" class="btn btn-success">{{__('Create post')}}</button>
+                <button type="submit" class="btn btn-success">{{__('Save post')}}</button>
                 <a href="{{route('posts.index')}}"
                    class="btn btn-outline-secondary float-right">{{__('Cancel')}}</a>
             </div>
@@ -34,6 +35,6 @@
 
 @push('js')
     @vite([
-        'resources/js/scripts.js',
+        'resources/js/posts.js',
     ])
 @endpush
